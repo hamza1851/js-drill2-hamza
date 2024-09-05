d3634c9e4586d4bf12787ced0d3c18597f363628
hamza1851 <hamza.dev011@gmail.com>
2024-09-05 12:55:33 +0530

    Add Solution 4

--- solutions.js
+++ solutions.js
@@ -35,4 +35,31 @@ function userWithMasterDeg(usersObj) {
     : userWithMasters
 }
 
-module.exports = { interestGames, userInGermany, userWithMasterDeg }
+function userBasedOnProgLang(usersObj) {
+  const userNames = Object.keys(usersObj)
+  const pythonRegex = /Python/i
+  const jsRegex = /Javascript/i
+  const goLangRegex = /Golang/i
+  const pythonUsers = []
+  const jsUsers = []
+  const golangUsers = []
+
+  for (let i = 0; i < userNames.length; i++) {
+    const userDesignation = usersObj[userNames[i]].desgination
+    if (pythonRegex.test(userDesignation)) {
+      pythonUsers.push(userNames[i])
+    } else if (goLangRegex.test(userDesignation)) {
+      golangUsers.push(userNames[i])
+    } else if (jsRegex.test(userDesignation)) {
+      jsUsers.push(userNames[i])
+    } else continue
+  }
+  return { pythonUsers, golangUsers, jsUsers }
+}
+
+module.exports = {
+  interestGames,
+  userInGermany,
+  userWithMasterDeg,
+  userBasedOnProgLang,
+}
--- test-solutions.js
+++ test-solutions.js
@@ -2,6 +2,7 @@ const {
   interestGames,
   userInGermany,
   userWithMasterDeg,
+  userBasedOnProgLang,
 } = require("./solutions")
 const { users } = require("./users")
 
@@ -22,3 +23,10 @@ const userWithMasters = userWithMasterDeg(users)
 for (let i = 0; i < userWithMasters.length; i++) {
   console.log(userWithMasters[i])
 }
+
+console.log("-------Question-4---------")
+
+const { pythonUsers, golangUsers, jsUsers } = userBasedOnProgLang(users)
+console.log(`Users who use Python: ${pythonUsers}`)
+console.log(`Users who use Golang: ${golangUsers}`)
+console.log(`Users who use JavaScript: ${jsUsers}`)
