7d95093b99217e308b76d02eb980c55620722d3c
hamza1851 <hamza.dev011@gmail.com>
2024-09-05 12:31:57 +0530

    Add Solution3

--- solutions.js
+++ solutions.js
@@ -13,4 +13,26 @@ function interestGames(usersObj) {
   }
 }
 
-module.exports = { interestGames }
+function userInGermany(usersObj) {
+  const userNames = Object.keys(usersObj)
+  const germanUser = []
+  for (let i = 0; i < userNames.length; i++) {
+    const nationality = usersObj[userNames[i]].nationality
+    if (nationality === "Germany") germanUser.push(userNames[i])
+  }
+  return germanUser.length === 0 ? "No German User Found" : germanUser
+}
+
+function userWithMasterDeg(usersObj) {
+  const userNames = Object.keys(usersObj)
+  const userWithMasters = []
+  for (let i = 0; i < userNames.length; i++) {
+    if (usersObj[userNames[i]].qualification === "Masters")
+      userWithMasters.push(userNames[i])
+  }
+  return userWithMasters.length === 0
+    ? "No user has Masters Degree"
+    : userWithMasters
+}
+
+module.exports = { interestGames, userInGermany, userWithMasterDeg }
--- test-solutions.js
+++ test-solutions.js
@@ -1,5 +1,24 @@
-const { interestGames } = require("./solutions")
+const {
+  interestGames,
+  userInGermany,
+  userWithMasterDeg,
+} = require("./solutions")
 const { users } = require("./users")
 
+console.log("-------Question-1---------")
 const names = interestGames(users)
 console.log(names)
+
+console.log("-------Question-2---------")
+
+const germanUser = userInGermany(users)
+for (let i = 0; i < germanUser.length; i++) {
+  console.log(germanUser[i])
+}
+
+console.log("-------Question-3---------")
+
+const userWithMasters = userWithMasterDeg(users)
+for (let i = 0; i < userWithMasters.length; i++) {
+  console.log(userWithMasters[i])
+}
